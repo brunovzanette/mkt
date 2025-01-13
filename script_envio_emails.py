0a79ae58ee6328abe7ac21d335121e22cade10e0
Bruno Zanette <zntbruno@gmail.com>
2025-01-13 10:22:01 -0300

    Update script_envio_emails.py

--- script_envio_emails.py
+++ script_envio_emails.py
@@ -5,8 +5,10 @@ import win32com.client as win32
 import tkinter as tk
 from tkinter import messagebox, filedialog
 
+
 # Variável global para armazenar o caminho do arquivo
 caminho_arquivo_excel = ""
+
 
 # Função para obter o caminho do arquivo 'clientes.xlsx'
 def obter_caminho_arquivo_excel():
@@ -17,6 +19,7 @@ def obter_caminho_arquivo_excel():
     
 
     return caminho_arquivo_excel
+
 
 # Função para permitir que o usuário selecione o arquivo 'clientes.xlsx'
 def selecionar_arquivo():
@@ -32,6 +35,7 @@ def selecionar_arquivo():
     else:
         messagebox.showerror("Erro", "Nenhum arquivo foi selecionado.")
 
+
 # Função para carregar dados dos clientes do Excel
 def carregar_dados_clientes():
     try:
@@ -40,8 +44,10 @@ def carregar_dados_clientes():
     except Exception as e:
         messagebox.showerror("Erro", f"Ocorreu um erro ao carregar o arquivo Excel: {e}")
 
+
 # Instância do Outlook
 outlook = win32.Dispatch('Outlook.Application')
+
 
 # Função para enviar e-mails
 def enviar_emails(assunto, corpo_email):
@@ -70,6 +76,7 @@ def enviar_emails(assunto, corpo_email):
         # Exibir mensagem de erro
         messagebox.showerror("Erro", f"Ocorreu um erro ao enviar os e-mails: {e}")
 
+
 # Função que pega os dados da interface e chama a função de envio
 def enviar_email():
     assunto = entry_assunto.get()
@@ -81,17 +88,21 @@ def enviar_email():
         # Chama a função de envio de e-mails com os dados fornecidos
         enviar_emails(assunto, corpo_email)
 
+
 # Criando a interface gráfica
 root = tk.Tk()
 root.title("ENVIOS DE E-MAILS EM MASSA")
+
 
 # Adicionando descrição explicativa
 label_descricao = tk.Label(root, text="Essa ferramenta permite enviar e-mails em massa para todos os seus contatos cadastrados no arquivo 'clientes.xlsx'.\
     O e-mail de envio será o configurado no Outlook do seu computador.", wraplength=400)
 label_descricao.pack(pady=10)
 
+
 # Tamanho da janela
 root.geometry("600x500")
+
 
 # Label e campo para o assunto
 label_assunto = tk.Label(root, text="Assunto do E-mail:")
@@ -99,19 +110,23 @@ label_assunto.pack(pady=5)
 entry_assunto = tk.Entry(root, width=65)
 entry_assunto.pack(pady=5)
 
+
 # Label e campo para o corpo do e-mail
 label_corpo_email = tk.Label(root, text="Corpo do E-mail (use {nome} para personalizar):")
 label_corpo_email.pack(pady=5)
 text_corpo_email = tk.Text(root, height=10, width=50)
 text_corpo_email.pack(pady=5)
 
+
 # Botão para selecionar o arquivo 'clientes.xlsx'
 botao_selecionar_arquivo = tk.Button(root, text="Selecionar Arquivo 'clientes.xlsx'", command=selecionar_arquivo, height=2, width=30)
 botao_selecionar_arquivo.pack(pady=10)
 
+
 # Botão para enviar o e-mail
 botao_enviar = tk.Button(root, text="Enviar E-mail para lista de contatos", command=enviar_email, height=3, width=50)
 botao_enviar.pack(pady=40)
+
 
 # Iniciar a interface
 root.mainloop()
